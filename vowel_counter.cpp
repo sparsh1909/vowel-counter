47d32b0e1ef9b054dd801cbef903a5d8866780ce
sparsh1909 <145043901+sparsh1909@users.noreply.github.com>
2023-11-29 13:37:00 +0530

    Update vowel_counter.cpp

--- vowel_counter.cpp
+++ vowel_counter.cpp
@@ -4,29 +4,37 @@
 
 using namespace std;
 
+// Function to count the number of vowels in a given word
 int countVowels(const string& word) {
+    // Set of vowels (both uppercase and lowercase)
     const unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
     int count = 0;
 
+    // Loop through each character in the word
     for (char c : word) {
+        // Check if the character is a vowel
         if (vowels.find(c) != vowels.end()) {
-            count++;
+            count++; // Increment the vowel count
         }
     }
 
-    return count;
+    return count; // Return the total count of vowels
 }
 
 int main() {
     string inputWord;
+
+    // Get input from the user
     cout << "Enter a word: ";
     cin >> inputWord;
 
+    // Check if the input word is empty
     if (inputWord.empty()) {
         cout << "The input string is empty.\n";
         return 0;
     }
 
+    // Call the function to count vowels and display the result
     int result = countVowels(inputWord);
     cout << "The number of vowels in '" << inputWord << "' is " << result << ".\n";
 
