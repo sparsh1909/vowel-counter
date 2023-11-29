b78f372e6d81028affc07ada9796d06e901db8a1
sparsh1909 <145043901+sparsh1909@users.noreply.github.com>
2023-11-29 20:55:08 +0530

    Update vowel_counter.cpp
    Added comments about code if there is sequence of words separated by spaces or sentence.

--- vowel_counter.cpp
+++ vowel_counter.cpp
@@ -40,3 +40,59 @@ int main() {
 
     return 0;
 }
+
+
+// // If there is a sequence of words or string containing words separated by spaces
+// #include <iostream>
+// #include <string>
+// #include <unordered_set>
+// #include <sstream> // Include the header for stringstream
+
+// using namespace std;
+
+// // Function to count the number of vowels in a given word
+// int countVowels(const string& word) {
+//     // Set of vowels (both uppercase and lowercase)
+//     const unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+//     int count = 0;
+
+//     // Loop through each character in the word
+//     for (char c : word) {
+//         // Check if the character is a vowel
+//         if (vowels.find(c) != vowels.end()) {
+//             count++; // Increment the vowel count
+//         }
+//     }
+
+//     return count; // Return the total count of vowels
+// }
+
+// int main() {
+//     string inputString;
+
+//     // Get input from the user
+//     cout << "Enter a string: ";
+//     getline(cin, inputString); // Use getline to get the entire line
+
+//     // Check if the input string is empty
+//     if (inputString.empty()) {
+//         cout << "The input string is empty.\n";
+//         return 0;
+//     }
+
+//     // Use a stringstream to extract words from the input string
+//     stringstream ss(inputString);
+//     string word;
+//     int totalVowels = 0;
+
+//     // Loop through each word in the input string
+//     while (ss >> word) {
+//         // Call the function to count vowels for each word
+//         totalVowels += countVowels(word);
+//     }
+
+//     // Display the result
+//     cout << "The total number of vowels in the string is " << totalVowels << ".\n";
+
+//     return 0;
+// }
